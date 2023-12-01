fda7689d35d79a0ab13beb86eb44e8b77f8a8b23
Kamoliddinbek <kamoliddinxudyberdiyev72@gmail.com>
2023-12-01 08:44:42 +0500

    Header && Contact

--- src/components/headers/Header.vue
+++ src/components/headers/Header.vue
@@ -1,9 +1,179 @@
 <template>
-  <div>
-    <h1>Header</h1>
+  <div class="w-full h-[183px] mx-auto">
+    <div class="flex justify-around bg-[#EBEFF3] gap-96 pl-8 pr-12">
+      <div class="flex gap-8 h-[40px] items-center">
+        <div class="flex gap-2">
+          <svg-icon type="mdi" :path="mdiMapMarkerOutline"></svg-icon>
+          <h3>Tashkent</h3>
+        </div>
+        <router-link to=""><h3>About Us</h3></router-link>
+        <router-link to=""><h3>Products</h3></router-link>
+        <router-link to=""><h3>Contacts</h3></router-link>
+      </div>
+      <div class="flex gap-5 items-center">
+        <h3>+998 (71) 123-45-67</h3>
+        <select class="bg-[#EBEFF3] border-none">
+          <option value="uz">Uz</option>
+          <option value="ru">Ru</option>
+        </select>
+      </div>
+    </div>
+    <div class="flex pl-32 pr-12 items-center mt-[20px]">
+      <div class="relative inline-block w-[170px]">
+        <img src="../../assets/images/logo.svg" alt="logo" class="" />
+        <h1
+          class="text-[#134E9B] text-[36px] font-black absolute left-[55px] top-[6px]"
+        >
+          Ashyo
+        </h1>
+      </div>
+
+      <div class="mt-3 ml-[53px]">
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          :ellipsis="false"
+          active-text-color="black"
+          popper-effect="true"
+          @select="handleSelect"
+        >
+          <el-sub-menu
+            index="2"
+            class="bg-[#134E9B] font-medium h-[48px] w-[160px] rounded-[6px]"
+            text-color="black"
+          >
+            <template #title
+              ><p class="text-[#fff] font-normal">Kategoriya</p></template
+            >
+            <el-sub-menu index="2-1"
+              ><template #title>item one</template>
+              <el-sub-menu index="2-1-1">
+                <template #title>item 1</template>
+                <el-menu-item index="2-1-1-1">item bir</el-menu-item>
+              </el-sub-menu>
+              <el-menu-item index="2-1-2"
+                >Smartfonlar va Aksasuerlar</el-menu-item
+              >
+              <el-menu-item index="2-1-3">item three</el-menu-item></el-sub-menu
+            >
+            <el-menu-item index="2-2">Smartfonlar va Aksasuerlar</el-menu-item>
+            <el-menu-item index="2-3">Kiryuvish mashinalari</el-menu-item>
+            <el-sub-menu index="2-4">
+              <template #title>item four</template>
+              <el-menu-item index="2-4-1">item one</el-menu-item>
+              <el-menu-item index="2-4-2">item two</el-menu-item>
+              <el-menu-item index="2-4-3">item three</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+        </el-menu>
+      </div>
+      <div class="ml-[10px]" style="position: relative">
+        <input
+          type="search"
+          placeholder="What are you looking for?"
+          class="w-[518px] h-[48px] rounded-[6px] bg-[#EBEFF3] pl-[26px] font-normal"
+          style="padding-right: 30px"
+        />
+        <button class="loop flex justify-center items-center text-[#fff]">
+          <svg-icon type="mdi" :path="mdiMagnify"></svg-icon>
+        </button>
+      </div>
+      <div class="flex gap-[15px] ml-[51px]">
+        <el-badge :value="2" class="item">
+          <button
+            class="w-[50px] h-[48px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+          >
+            <svg-icon type="mdi" :path="mdiScaleUnbalanced"></svg-icon>
+          </button>
+        </el-badge>
+        <el-badge :value="11" class="item">
+          <button
+            class="w-[50px] h-[48px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+          >
+            <svg-icon type="mdi" :path="mdiCardsHeartOutline"></svg-icon>
+          </button>
+        </el-badge>
+        <el-badge :value="7" class="item">
+          <button
+            class="w-[50px] h-[48px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+          >
+            <svg-icon type="mdi" :path="mdiShoppingOutline"></svg-icon>
+          </button>
+        </el-badge>
+        <button
+          class="w-[50px] h-[48px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+        >
+          <svg-icon type="mdi" :path="mdiAccountOutline"></svg-icon>
+        </button>
+      </div>
+    </div>
+    <div class="list flex gap-[36px] items-center mt-[20px] ml-[155px]">
+      <p>Aksiyalar</p>
+      <p>Smartfonlar</p>
+      <p>Noutbooklar</p>
+      <p>Kondetsionerlar</p>
+      <p>Telivizorlar</p>
+      <p>Muzlatgichlar</p>
+      <p>Kiryuvish mashinalari</p>
+      <p>Telivizorlar</p>
+      <p>Kiryuvish mashinalari</p>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//@ts-ignore
+import SvgIcon from "@jamescoyle/vue-icon";
+import {
+  mdiMapMarkerOutline,
+  mdiMagnify,
+  mdiScaleUnbalanced,
+  mdiCardsHeartOutline,
+  mdiShoppingOutline,
+  mdiAccountOutline,
+} from "@mdi/js";
+import { ref } from "vue";
 
-<style scoped></style>
+const activeIndex = ref("2");
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+</script>
+
+<style scoped>
+.flex-grow {
+  flex-grow: 1;
+}
+
+.loop {
+  position: absolute;
+  right: 0px;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 60px;
+  height: 48px;
+  border-radius: 6px;
+  background-color: #134e9b;
+}
+.el-menu-item {
+  color: black;
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.list p {
+  color: #545d6a;
+  text-align: center;
+
+  /* Roboto/reguler16px */
+  /* font-family: Roboto; */
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 21px; /* 131.25% */
+}
+</style>
---
+++ src/views/user/Contact.vue
@@ -0,0 +1,279 @@
+<template>
+  <div>
+    <h1>Contact Page</h1>
+    <Header />
+  </div>
+
+  <div class="mt-[21px] ml-[150px]">
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">
+        <span class="text-[#B6BABF] font-normal">Bosh sahifa</span>
+      </el-breadcrumb-item>
+      <el-breadcrumb-item
+        ><a href="/contact"
+          ><span class="text-[#B6BABF] font-normal">Qayta aloqa</span></a
+        ></el-breadcrumb-item
+      >
+      <el-breadcrumb separator="/"></el-breadcrumb>
+    </el-breadcrumb>
+    <div class="mt-[22px]">
+      <p class="font-bold text-3xl">Qayta aloqa</p>
+      <p class="text-[#545D6A] font-base font-normal mt-[16px]">
+        Bizning ishimiz haqidagi fikir mulohazalaringiz bilan bo’lishing yoki
+        izohlar maydonida o’zingizni qiziqtirgan <br />
+        savolingizni yo’llang
+      </p>
+      <div class="mt-[35px] flex gap-[30px]">
+        <div class="flex flex-col">
+          <label
+            for="name"
+            id="name"
+            class="text-[#848B93] text-xs ml-[10px] pb-[5px]"
+            >Ism</label
+          >
+          <input
+            type="text"
+            id="name"
+            class="w-[330px] h-[56px] bg-[#EBEFF3] rounded-[6px] pl-5"
+          />
+        </div>
+        <div class="flex flex-col">
+          <label
+            for="surname"
+            id="surname"
+            class="text-[#848B93] text-xs ml-[10px] pb-[5px]"
+            >Familiya</label
+          >
+          <input
+            type="text"
+            id="surname"
+            class="w-[330px] h-[56px] bg-[#EBEFF3] rounded-[6px] pl-5"
+          />
+        </div>
+      </div>
+      <div class="mt-[30px] flex gap-[30px]">
+        <div class="flex flex-col">
+          <label
+            for="phone"
+            id="phone"
+            class="text-[#848B93] text-xs ml-[10px] pb-[5px]"
+            >Telefon raqam</label
+          >
+          <input
+            type="text"
+            id="phone"
+            class="w-[330px] h-[56px] bg-[#EBEFF3] rounded-[6px] pl-5"
+          />
+        </div>
+        <div class="flex flex-col">
+          <label
+            for="email"
+            id="email"
+            class="text-[#848B93] text-xs ml-[10px] pb-[5px]"
+            >Elektron pochta</label
+          >
+          <input
+            type="email"
+            id="email"
+            class="w-[330px] h-[56px] bg-[#EBEFF3] rounded-[6px] pl-5"
+          />
+        </div>
+      </div>
+      <div class="mt-[30px] flex flex-col w-[690px]">
+        <label
+          for="comment"
+          id="comment"
+          class="text-[#848B93] text-xs ml-[10px] pb-[5px]"
+          >O’zingizni fikrlangizni qoldiring</label
+        >
+        <textarea
+          name="comment"
+          id="comment"
+          cols="10"
+          rows="6"
+          class="bg-[#EBEFF3] rounded-[6px] pl-5"
+        ></textarea>
+      </div>
+      <button
+        class="w-[690px] bg-[#15509E] rounded-[6px] text-[#fff] py-[18px] mt-[30px] text-base"
+      >
+        Yuborish
+      </button>
+    </div>
+  </div>
+  <div
+    class="relative w-full h-[450px] mt-[114px]"
+    style="max-height: 450px; overflow: hidden"
+  >
+    <img
+      src="../../assets/images/map.png"
+      alt="map"
+      style="width: 100%; height: auto"
+    />
+    <div
+      class="absolute top-[13px] left-[150px] p-4 bg-white w-[455px] h-[422px]"
+    >
+      <div class="p-[50px]">
+        <p class="text-lg font-bold">OOO “Ashyo”</p>
+        <p class="text-xs mt-[20px]">
+          Telefon raqam: <span class="text-base">+998 71 123 45 56</span>
+        </p>
+        <p class="text-xs mt-[15px]">
+          Elektron pochta: <span class="text-base">ashyo@gmail.com</span>
+        </p>
+        <p class="text-xs mt-[15px]">Manzilimiz:</p>
+        <p class="text-base mt-[10px]">
+          100052, O‘zbekiston Respublikasi, Toshkent shahri, Bodomzor yo‘li
+          1-tor ko‘chasi, 72
+        </p>
+        <p class="text-xs mt-[15px]">Ijtimoiy tarmoqlarimiz</p>
+        <div class="flex gap-[6px] mt-[10px]">
+          <button
+            class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+          >
+            <img src="../../assets/images/facebook.svg" alt="" />
+          </button>
+          <button
+            class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+          >
+            <img src="../../assets/images/youtube.svg" alt="" />
+          </button>
+          <button
+            class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+          >
+            <img src="../../assets/images/telegram.svg" alt="" />
+          </button>
+          <button
+            class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+          >
+            <img src="../../assets/images/twitter.svg" alt="" />
+          </button>
+          <button
+            class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+          >
+            <img src="../../assets/images/instagram.png" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="absolute top-[13px] left-[626px]">
+      <select class="w-[344px] h-[64px] rounded px-4">
+        <option class="" value="" selected hidden>
+          <p class="m-3">Do'konlarimizni izlang</p>
+        </option>
+
+        <option value="chilonzor">Chilonzor tumani</option>
+        <option value="olmazor">Olmazor tumani</option>
+        <option value="shayxontohur">Shayxontohur tumani</option>
+        <option value="mirzo">Mirzo Ulug'bek tumani</option>
+        <option value="sergeli">Sergeli tumani</option>
+        <option value="yashnobod">Yashnobod tumani</option>
+        <option value="yakkasaroy">Yakkasaroy tumani</option>
+        <option value="yunusobod">Yunusobod tumani</option>
+        <option value="uchtepa">Uchtepa tumani</option>
+      </select>
+    </div>
+  </div>
+  <div class="flex flex-row pt-[70px] pb-[141px] gap-[130px] ml-[150px]">
+    <div class="flex gap-[6px] mt-[10px] flex-col">
+      <p class="text-xl font-bold">Bizning ijtimoiy tarmoqlarda</p>
+      <div class="flex gap-[10px] mt-[21.5px] mb-[39.3px]">
+        <button
+          class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+        >
+          <img src="../../assets/images/facebook.svg" alt="" />
+        </button>
+        <button
+          class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+        >
+          <img src="../../assets/images/youtube.svg" alt="" />
+        </button>
+        <button
+          class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+        >
+          <img src="../../assets/images/telegram.svg" alt="" />
+        </button>
+        <button
+          class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+        >
+          <img src="../../assets/images/twitter.svg" alt="" />
+        </button>
+        <button
+          class="w-[44px] h-[40px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+        >
+          <img src="../../assets/images/instagram.png" alt="" />
+        </button>
+      </div>
+      <p class="text-xl font-bold">Mobil ilovani yuklab oling</p>
+      <div class="flex gap-[12px] mt-[12px]">
+        <button
+          class="w-[188px] h-[66px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+        >
+          <img src="../../assets/images/apple.svg" alt="apple" /><span
+            class="text-xl font-bold ml-3"
+            >App Store</span
+          >
+        </button>
+        <button
+          class="w-[188px] h-[66px] flex justify-center items-center rounded-[6px] bg-[#EBEFF3]"
+        >
+          <img src="../../assets/images/googleplay.svg" alt="apple" /><span
+            class="text-xl font-bold ml-3"
+            >Google Play</span
+          >
+        </button>
+      </div>
+      <p class="text-xs mt-[57px]">
+        © 2022 Ashyo ro’yhatdan otgan litsenzalangan bu brend.
+      </p>
+    </div>
+    <div class="mt-[10px]">
+      <p class="text-xl font-bold mb-[18px]">Menyu</p>
+      <p class="mb-[12px]">Ashyo haqida</p>
+      <p class="mb-[12px]">Foydalanish shartlari</p>
+      <p class="mb-[12px]">Maxfiylik va hafsizlik siyosati</p>
+      <p class="mb-[12px]">
+        Mahsulotlarni va tovarlarni <br />
+        qaytarish siyosati
+      </p>
+      <p>Biz bilan aloqa</p>
+    </div>
+    <div class="mt-[10px]">
+      <p class="text-xl font-bold mb-[18px]">Aloqa</p>
+      <p class="text-2xl font-bold text-[gray] mb-[32px]">
+        +998 (71) 123-45-67
+      </p>
+      <p class="text-base mb-[12px]">Savolingiz bormi?</p>
+      <div class="" style="position: relative">
+        <input
+          type="search"
+          placeholder=" O’zingiz qiziqtirgan savollarni bering"
+          class="w-[314px] h-[54px] rounded-[6px] bg-[#EBEFF3] pl-[26px] font-normal text-xs"
+          style="padding-right: 30px"
+        />
+        <button class="loop flex justify-center items-center">
+          <svg-icon type="mdi" :path="mdiMessageOutline"></svg-icon>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from "@/components/headers/Header.vue";
+//@ts-ignore
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiMessageOutline } from "@mdi/js";
+</script>
+
+<style scoped>
+.loop {
+  position: absolute;
+  right: 0px;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 60px;
+  height: 48px;
+  border-radius: 6px;
+}
+</style>
